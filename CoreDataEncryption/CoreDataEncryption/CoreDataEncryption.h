074bdcf85b9b528a30c0a8ffe447da5f6596dc45
Ravi Prakash <raviprakash.xpd54@gmail.com>
2018-07-12 14:39:28 +0800

    Setup Project
    1. Add Submodule dependencies
    2. Set environment flags explained in README file

---
+++ CoreDataEncryption/CoreDataEncryption/CoreDataEncryption.h
@@ -0,0 +1,13 @@
+//
+//  CoreDataEncryption.h
+//  CoreDataEncryption
+//
+//  Created by Ravi Prakash on 9/7/2018.
+//  Copyright © 2018 Ravi Prakash. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CoreDataEncryption : NSObject
+
+@end
---
+++ CoreDataEncryption/CoreDataEncryption/CoreDataEncryption.m
@@ -0,0 +1,13 @@
+//
+//  CoreDataEncryption.m
+//  CoreDataEncryption
+//
+//  Created by Ravi Prakash on 9/7/2018.
+//  Copyright © 2018 Ravi Prakash. All rights reserved.
+//
+
+#import "CoreDataEncryption.h"
+
+@implementation CoreDataEncryption
+
+@end
